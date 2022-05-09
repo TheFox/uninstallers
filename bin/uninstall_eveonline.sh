a6231ef3bcc2d54e3480fba669503ae2a83ffa5c
Christian Mayer <christian@fox21.at>
2022-05-09 10:11:09 +0200

    EVE Online

---
+++ bin/uninstall_eveonline.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+###
+### uninstall_eveonline.sh - Uninstaller
+###
+### Usage:
+###   uninstall_eveonline.sh [-h]
+###
+### Options:
+###   -h                  Show this message.
+
+NO_COLOR='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+help() {
+    head -50 "$0" | grep '^###' | sed 's/^###//; s/^ //'
+}
+
+if [[ "$1" == -h ]]; then
+    help
+    exit 1
+fi
+
+which -s brew && brew uninstall eve-launcher
+
+sudo rm -rf '/Applications/EVE Launcher.app'
+
+rm -rf ~/'Library/Saved Application State/com.ccpgames.evelauncher.savedState'
+rm -rf ~/'Library/Application Support/CCP/EVE'
+rm -rf ~/'Library/Application Support/EVE Online'
+rm -rf ~/Library/Preferences/com.ccpgames.EVE.plist
