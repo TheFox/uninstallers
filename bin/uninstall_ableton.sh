ff69d07b9f3a0309475b959ddad22db48967353d
Christian Mayer <christian@fox21.at>
2023-12-24 10:25:27 +0100

    Ableton

--- bin/uninstall_ableton.sh
+++ bin/uninstall_ableton.sh
@@ -23,12 +23,17 @@ if [[ "$1" == -h ]]; then
     exit 1
 fi
 
-which -s brew && brew uninstall ableton-live-lite
+which -s brew && brew uninstall ableton-live-lite ableton-live-lite10
 
 sudo rm -vrf '/Applications/Ableton Live 11 Trial.app'
+sudo rm -vrf '/Applications/Ableton Live 11 Lite.app'
 
 rm -vrf ~/'Library/Application Support/Ableton'
+rm -vrf ~/Library/'Application Support/CrashReporter/Ableton Index_'*.plist
 rm -vrf ~/'Library/Saved Application State/com.ableton.live.savedState'
 rm -vrf ~/'Library/Preferences/Ableton/Live 10.1.30'
 rm -vrf ~/'Library/Preferences/Ableton/Live 11.0.6'
 rm -vrf ~/Library/Preferences/com.ableton.live.plist
+rm -vrf ~/Library/Preferences/Ableton
+rm -vrf ~/Library/WebKit/com.ableton.live
+rm -vrf ~/Library/HTTPStorages/'Ableton Plugin Scanner'
