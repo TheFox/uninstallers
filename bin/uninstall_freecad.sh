0ed3df8afb09cae0081a71f4848913ab12daccb7
Christian Mayer <christian@fox21.at>
2024-08-16 17:08:08 +0200

    FreeCAD

---
+++ bin/uninstall_freecad.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+###
+### uninstall.sh - Uninstaller
+###
+### Usage:
+###   uninstall.sh [-h]
+###
+### Options:
+###   -h                  Show this message.
+
+NO_COLOR='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+help() {
+    head -50 "$0" | grep '^###' | sed 's/^###//; s/^ //'
+}
+
+if [[ "$1" == -h ]]; then
+    help
+    exit 1
+fi
+
+set -x
+
+which -s brew && brew uninstall openscad
+
+sudo rm -rf /Applications/FreeCAD.app
+
+rm -rf ~/Library/'Application Support'/FreeCAD
+rm -rf ~/Library/Preferences/FreeCAD
+rm -rf ~/Library/Preferences/freecad.plist
+rm -rf ~/Library/Preferences/com.freecad.FreeCAD.plist
+rm -rf ~/Library/Caches/FreeCAD
