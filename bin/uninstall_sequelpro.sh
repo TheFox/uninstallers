913bfaeb7f4153229de980e19aa6c4aec1df1699
Christian Mayer <christian@fox21.at>
2024-09-22 21:02:20 +0200

    SequelPro

--- bin/uninstall_sequelpro.sh
+++ bin/uninstall_sequelpro.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 ###
-### uninstall_sequelpro.sh - Uninstaller
+### uninstall.sh - Uninstaller
 ###
 ### Usage:
-###   uninstall_sequelpro.sh [-h]
+###   uninstall.sh [-h]
 ###
 ### Options:
 ###   -h                  Show this message.
