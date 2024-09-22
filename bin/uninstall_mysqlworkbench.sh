1d278bfaae1c4cb8b330004b38adeea7192556af
Christian Mayer <christian@fox21.at>
2024-09-22 21:01:49 +0200

    MySQLWorkbench

---
+++ bin/uninstall_mysqlworkbench.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+###
+### uninstall.sh - Uninstaller
+###
+### Usage:
+###   uninstall.sh [-h]
+###
+### Options:
+###   -h                  Show this message.
+
+NO_COLOR='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+help() {
+    head -50 "$0" | grep '^###' | sed 's/^###//; s/^ //'
+}
+
+if [[ "$1" == -h ]]; then
+    help
+    exit 1
+fi
+
+which -s brew && brew uninstall mysqlworkbench
+
+rm -rf ~/Library/'Saved Application State'/com.oracle.workbench.MySQLWorkbench.savedState
+rm -rf ~/Library/Preferences/com.oracle.workbench.MySQLWorkbench.plist
