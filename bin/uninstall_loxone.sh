e30bce446db0329f31fac24dd9aa669854c55ddf
Christian Mayer <christian.mayer@digitalsunray.com>
2026-04-26 12:20:06 +0200

    Loxone

---
+++ bin/uninstall_loxone.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+###
+### uninstall.sh - Uninstaller
+###
+### Usage:
+###   uninstall.sh [-h]
+###
+### Options:
+###   -h                  Show this message.
+
+NO_COLOR='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+help() {
+    head -50 "$0" | grep '^###' | sed 's/^###//; s/^ //'
+}
+
+if [[ "$1" == -h ]]; then
+    help
+    exit 1
+fi
+
+sudo rm -vrf /Applications/Loxone.app
+
+rm -rf ~/Library/Application Support/Loxone
+rm -rf ~/Library/Preferences/com.loxone.groot.plist
+sudo rm -rf ~/Library/Group Containers/6RTZZ2378K.com.loxone.groot
